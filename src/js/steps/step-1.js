9cc52f495a660883193e30c536b9e5d7747ae917
Liliia_Sydorenko <lilya.sidorenko1@gmail.com>
2017-06-20 15:23:54 +0300

    Fix animate

--- src/js/steps/step-1.js
+++ src/js/steps/step-1.js
@@ -2,16 +2,19 @@ import {enabledButton} from '../settings/enabled-button';
 
 export const step1Init = () => {
     window.onload = function() {
+        setTimeout(function () {
+            let timeLite = new TimelineLite({paused: true}),
+                img = document.getElementById('js-animate-logo');
 
-        let timeLite = new TimelineLite({paused: true}),
-            img = document.getElementById('js-animate-logo');
+            for (let source = 0; source <= 120; source++) {
+                let validSource = (source < 10) ? (`00${source}`) : (source < 100) ? (`0${source}`) : source;
+                timeLite.set(img, {attr: {src: `assets/animation/${validSource}.png`}}, source);
+            }
 
-        for (let source = 0; source <= 120; source++) {
-            let validSource = (source < 10) ? (`00${source}`) : (source < 100) ? (`0${source}`) : source;
-            timeLite.set(img, {attr: {src: `assets/animation/${validSource}.png`}}, source);
-        }
+            TweenLite.to(timeLite, 3, {progress: 1, ease: Power1.easeInOut});
+            enabledButton(2000);
+        },500)
 
-        TweenLite.to(timeLite, 3, {progress: 1, ease: Power1.easeInOut});
-        enabledButton(2000);
+
     };
 };
