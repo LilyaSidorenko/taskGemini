04c6428c9b961338173f41b5ce52362d6c7ddc4b
Liliia_Sydorenko <lilya.sidorenko1@gmail.com>
2017-06-20 16:49:05 +0300

    Revert changes

--- src/js/steps/step-1.js
+++ src/js/steps/step-1.js
@@ -1,20 +1,16 @@
 import {enabledButton} from '../settings/enabled-button';
 
 export const step1Init = () => {
-    window.onload = function() {
-        setTimeout(function () {
-            let timeLite = new TimelineLite({paused: true}),
-                img = document.getElementById('js-animate-logo');
 
-            for (let source = 0; source <= 120; source++) {
-                let validSource = (source < 10) ? (`00${source}`) : (source < 100) ? (`0${source}`) : source;
-                timeLite.set(img, {attr: {src: `assets/animation/${validSource}.png`}}, source);
-            }
+    let timeLite = new TimelineLite({paused: true}),
+        img = document.getElementById('js-animate-logo');
 
-            TweenLite.to(timeLite, 3, {progress: 1, ease: Power1.easeInOut});
-            enabledButton(2000);
-        },500)
+    for (let source = 0; source <= 120; source++) {
+        let validSource = (source < 10) ? (`00${source}`) : (source < 100) ? (`0${source}`) : source;
+        timeLite.set(img, {attr: {src: `assets/animation/${validSource}.png`}}, source);
+    }
 
+    TweenLite.to(timeLite, 3, {progress: 1, ease: Power1.easeInOut});
+    enabledButton(2000);
 
-    };
 };
